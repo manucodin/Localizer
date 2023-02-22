07a3e8b6846ed6dc2882f5d6b736d052b6e64b0e
Manu Rodriguez <manurs93@gmail.com>
2023-02-22 09:17:37 +0100

    Cleaned commands and move to separated files

--- Sources/Localizer/Localizer.swift
+++ Sources/Localizer/Localizer.swift
@@ -4,30 +4,12 @@ import Rainbow
 
 @main
 struct Localizer: AsyncParsableCommand {
-    public static var configuration = CommandConfiguration(abstract: "Search and list all string in localizable.string")
-    
-    @Option(name: .shortAndLong, help: "Localize strings file path")
-    private var localizableFilePath: String
-    
-    @Option(name: .shortAndLong, parsing: .upToNextOption, help: "Paths to search strings localizables")
-    private var searchPaths: [String]
-    
-    mutating func run() async throws {
-        let parameters = Parameters(
-            localizableFilePath: localizableFilePath,
-            searchPaths: searchPaths
-        )
-        
-        do {
-            try await LocalizableMatcher(parameters: parameters).match()
-            print("Success".green)
-        } catch let error {
-            if let localizedError = error as? LocalizerError {
-                print(localizedError.description.red)
-            } else {
-                print(error.localizedDescription)
-            }
-            Localizer.exit(withError: error)
-        }
-    }
+    public static var configuration = CommandConfiguration(
+        abstract: "🔍 Search unlocalized strings on your project easily and quickly",
+        version: "1.0.0",
+        subcommands: [
+            Compare.self
+        ],
+        defaultSubcommand: Compare.self
+    )
 }
