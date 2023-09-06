1781f3e087abd020b164d86f73869e983435869b
manucodin <manurs93@gmail.com>
2023-09-06 10:52:21 +0200

    Updated Formula version to 1.1.0

--- Formula/localizer.rb
+++ Formula/localizer.rb
@@ -1,7 +1,7 @@
 class Localizer < Formula
     desc "Localizer is a simple tool for search your strings not localized in your project."
     homepage "https://github.com/manucodin/Localizer"
-    version "1.0.7"
+    version "1.1.0"
     url "https://github.com/manucodin/Localizer/releases/download/#{version}/localizer.zip"
   
     def install
